01545f515bb994736111eaadc55a702eb14654c4
Vedant Agarwal <agarwal23vedant@gmail.com>
2026-06-22 18:13:05 +0530

    Added Aubscription Model

---
+++ backend/src/models/subscription.models.js
@@ -0,0 +1,14 @@
+import mongoose, { Mongoose } from "mongoose";
+
+const subscriptionSchema = new mongoose.Schema({
+    subscriber:{
+        type: mongoose.Schema.Types.ObjectId, // one who is subscribing 
+        ref: "User"
+    },
+    channel:{
+        type:mongoose.Schema.Types.ObjectId, // one to 
+        ref:"User"
+    },
+},{timestamps:true});
+
+export const Subscription = mongoose.model("Subscription",subscriptionSchema);
